2629b3059c7d5492f994c39ab2971477fa079b99
aymanfarhat <ayman.farhat1@gmail.com>
2013-06-06 21:30:44 +0300

    Remove document.ready and tidy the JSON output

--- example/static/main.js
+++ example/static/main.js
@@ -1,29 +1,27 @@
-$(document).ready(function() { 
 $('#urlForm').submit(function() {
-        var form = $(this);
+    var form = $(this);
 
-        var page_url = form.find(".urlBox").val();
-        var page_headers = form.find("input[type='checkbox']").is(':checked');
-        
-        var request = $.ajax({
-            type: form.prop('method'),
-            url: window.location.protocol+'//'+window.location.host+"/crossdomain",
-            data: {url:page_url, headers: page_headers},
+    var page_url = form.find(".urlBox").val();
+    var page_headers = form.find("input[type='checkbox']").is(':checked');
+    
+    var request = $.ajax({
+        type: form.prop('method'),
+        url: window.location.protocol+'//'+window.location.host+"/crossdomain",
+        data: {url:page_url, headers: page_headers},
 
-            beforeSend: function() {
-                form.find('.urlBox').prop("disabled",true); 
-                form.find('.btn').addClass("disabled");
-                form.find('.btn i').addClass("icon-spin");
-            }
-        });
+        beforeSend: function() {
+            form.find('.urlBox').prop("disabled",true); 
+            form.find('.btn').addClass("disabled");
+            form.find('.btn i').addClass("icon-spin");
+        }
+    });
 
-        request.done(function(reply){
-            $('.reply-text').text(JSON.stringify(reply)); 
-            form.find('.urlBox').prop("disabled",false); 
-            form.find('.btn').removeClass("disabled");
-            form.find('.btn i').removeClass("icon-spin");
-        });
+    request.done(function(reply){
+        $('.reply-text').text(JSON.stringify(reply,null,4)); 
+        form.find('.urlBox').prop("disabled",false); 
+        form.find('.btn').removeClass("disabled");
+        form.find('.btn i').removeClass("icon-spin");
+    });
 
-        return false; 
-    }); 
-});
+    return false; 
+}); 
